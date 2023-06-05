5e19a393a38c62517a0caa9782063decb9830a92
chanthornAcademy <hoeurnchanthorn@gmail.com>
2023-06-05 11:38:00 +0700

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+# print("hello" + input("What is your name? "))
+# get string length from user input
+# print(len(input("What is your name? ")))
+
+# name = input("What is your name? ")
+# print(name + " has " + str(len(name)) + " characters.")
+
+a = input("a: ")
+b = input("b: ")
+
+# swap a and b
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
